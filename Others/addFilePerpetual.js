681d362efe063c84a8b05744c9feba7f7e67440a
Guilherme Nazareth <GNG@Guilhermes-MacBook-Pro.local>
2019-11-22 16:28:37 +1000

    other

--- Others/addFilePerpetual.js
+++ Others/addFilePerpetual.js
@@ -3,7 +3,6 @@ const scansDAO = require('../Database/DAO/scans');
 module.exports = {
     add: async (ticket) => {
         // const ticket = await scansDAO.getByTicket(ticket)
-        
     },
     
 }
